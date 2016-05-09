0209595a1abe5c6cfb180f687a2f770e23112470
AugustRush <liupingwei30@gmail.com>
2016-05-09 18:56:44 +0800

    first commit

---
+++ Sources/Animator.swift
@@ -0,0 +1,17 @@
+//
+//  Animator.swift
+//  StellarDemo
+//
+//  Created by AugustRush on 5/7/16.
+//  Copyright © 2016 August. All rights reserved.
+//
+
+import UIKit
+
+class Animator {
+   static let shared = Animator()
+    
+    init() {
+    
+    }
+}
---
+++ StellarDemo/StellarDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  StellarDemo
+//
+//  Created by AugustRush on 5/7/16.
+//  Copyright © 2016 August. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func button1Cliked(sender: AnyObject) {
+        
+    }
+}
+
