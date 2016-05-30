6e2962f61d814bc5d72eceaff5749f0c853f2468
AugustRush <liupingwei30@gmail.com>
2016-05-30 19:10:26 +0800

    add Title png

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Stellar
+<img src="https://github.com/AugustRush/Stellar/blob/master/title.png">
+
 A fantastic Physical animation library for swift(Not Just Spring !!!), it is base on UIDynamic and extension to it, friendly APIs make you use it or custom your own animation very easily!
 
 ## On display
@@ -37,7 +39,7 @@ A fantastic Physical animation library for swift(Not Just Spring !!!), it is bas
 - Snap
 - Attachment
 - Push
-- Collsion
+- Collsion(come soon)
 
 ### Animatable type
 
--- StellarDemo/StellarDemo/MyPlayground.playground/Contents.swift
+++ StellarDemo/StellarDemo/MyPlayground.playground/Contents.swift
@@ -6,36 +6,34 @@ let container = UIView(frame: CGRectMake(0,0,600,600))
 
 container.backgroundColor = UIColor(red: 0.97,green: 0.97,blue: 0.97,alpha: 1.0)
 
-//let ball = Ball(frame: CGRectMake(10,10,80,80))
-//ball.backgroundColor = UIColor.redColor()
-//container.addSubview(ball)
-//
-//ball.moveX(100).snap(0.6).moveY(100).snap()
-//    .then().makeColor(UIColor.blackColor()).duration(2).moveX(100).duration(1)
-//    .animate()
+let ball = Ball(frame: CGRectMake(10,10,80,80))
+ball.backgroundColor = UIColor.redColor()
+container.addSubview(ball)
+
+ball.moveX(100).moveY(100).duration(1).animate()
 
 XCPlaygroundPage.currentPage.liveView = container
 
-
-
-100.0.animateTo(300, duration: 1, delay: 0.0, type: .SwiftOut, autoreverses: false, repeatCount: 0, render: { (d) in
-    let y = d
-    }, completion: nil)
-
-
-100.0.pushedTo(300, render: { (d) in
-    let y = d
-    }, completion: nil)
-
-
-100.0.snapTo(150, damping: 0.4, render: { (d) in
-    let t = d
-    }, completion: nil)
-
-100.0.attachmentTo(200, damping: 0.3, frequency: 1, render: { (d) in
-    let y = d
-    }, completion: nil)
-
-//100.0.fallTo(200, magnitude: 0.6, render: { (d) in
+////basic curve
+//
+//100.0.animateTo(300, duration: 1, delay: 0.0, type: .SwiftOut, autoreverses: false, repeatCount: 0, render: { (d) in
 //    let y = d
 //    }, completion: nil)
+//
+//
+//100.0.pushedTo(300, render: { (d) in
+//    let y = d
+//    }, completion: nil)
+//
+//
+//100.0.snapTo(150, damping: 0.4, render: { (d) in
+//    let t = d
+//    }, completion: nil)
+//
+//100.0.attachmentTo(200, damping: 0.3, frequency: 1, render: { (d) in
+//    let y = d
+//    }, completion: nil)
+//
+////100.0.fallTo(200, magnitude: 0.6, render: { (d) in
+////    let y = d
+////    }, completion: nil)
